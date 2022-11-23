6dbfd86c55be167d9d0b82b81c939f9f267b28ff
邓明维 <3561294402@qq.com>
2022-11-23 11:28:12 +0800

    scala-包-导入包

---
+++ src/com/study/oop/twelve/scriptFile.scala
@@ -0,0 +1,14 @@
+//package com.study.oop.twelve {
+//  package child {
+//  }
+//  package object child {
+//    var name = "张三"
+//    def sayHello(): Unit = println("Hello World!")
+//  }
+//  object PackageObjectDemo {
+//    def main(args: Array[String]): Unit = {
+//      println(child.name)
+//      child.sayHello()
+//    }
+//  }
+}
