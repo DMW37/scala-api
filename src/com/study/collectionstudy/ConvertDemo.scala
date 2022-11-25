70e06e74027bb6a2b21caab49310930cd8db8ca7
邓明维 <3561294402@qq.com>
2022-11-25 20:32:11 +0800

    scala-集合-Traversable-类型转换

---
+++ src/com/study/collectionstudy/ConvertDemo.scala
@@ -0,0 +1,16 @@
+package com.study.collectionstudy
+
+/**
+* 类型转换
+*/
+object ConvertDemo {
+  def main(args: Array[String]): Unit = {
+    // 定义一个 Traversable 集合，包含 1, 2, 3, 4, 5, 6
+    val t = (1 to 6).toTraversable
+    // 将集合分别转成数组、列表、集这三种形式，并打印结果
+    println(t.toArray) // [I@7e0babb1
+    println(t.toList) // List(1, 2, 3, 4, 5, 6)
+    println(t.toSet) // Set(5, 1, 6, 2, 3, 4)
+    println(t.toSeq)
+ }
+}
