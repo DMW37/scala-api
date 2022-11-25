3c3a7491b452d9ee006618117e7d6895a97b6ae6
邓明维 <3561294402@qq.com>
2022-11-25 20:08:45 +0800

    scala-集合-Traversable-集合拼接concat

---
+++ src/com/study/collectionstudy/ConcatDemo.scala
@@ -0,0 +1,17 @@
+package com.study.collectionstudy
+
+/**
+* 拼接集合
+*/
+object ConcatDemo {
+  def main(args: Array[String]): Unit = {
+    // 已知有三个 Traversable 集合，分别存储(11, 22, 33), (44, 55), (66, 77, 88, 99)元素
+    val t1 = Traversable(11, 22, 33)
+    val t2 = Traversable(44, 55)
+    val t3 = Traversable(66, 77, 88, 99)
+    // 通过 concat 方法拼接上述三个集合
+    val result = Traversable.concat(t1,t2,t3)
+    // result = List(11, 22, 33, 44, 55, 66, 77, 88, 99)
+    println(s"result = ${result}")
+ }
+}
