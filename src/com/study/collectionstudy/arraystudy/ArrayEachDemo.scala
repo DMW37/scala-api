46561db8db179d03f7f1a6069661004fa40d2f48
邓明维 <3561294402@qq.com>
2022-11-26 09:26:09 +0800

    scala-集合-array-数组遍历

---
+++ src/com/study/collectionstudy/arraystudy/ArrayEachDemo.scala
@@ -0,0 +1,38 @@
+package com.study.collectionstudy.arraystudy
+
+/**
+ * @author: 邓明维
+ * @date: 2022/11/26
+ * @description:
+ */
+object ArrayEachDemo {
+  def main(args: Array[String]): Unit = {
+    val arr = Array(1, "2", 3.14, true, null)
+    // to
+    for (i <- 0 to arr.length - 1) {
+      println(s"arr(i) = ${arr(i)}")
+    }
+    println("=" * 20, "华丽的分割线", "=" * 20)
+    // until 左闭右开
+    for (i <- 0 until arr.length) {
+      println(s"arr(i) = ${arr(i)}")
+    }
+    println("=" * 20, "华丽的分割线", "=" * 20)
+    // 遍历索引
+    for (i <- arr.indices) {
+      println(s"arr(i) = ${arr(i)}")
+    }
+    println("=" * 20, "华丽的分割线", "=" * 20)
+    // 遍历元素
+    for (elem <- arr) {
+      println(elem)
+    }
+    println("=" * 20, "华丽的分割线", "=" * 20)
+    // foreach
+    arr.foreach(x => println(x))
+    arr.foreach(_ => println(_))
+    arr.foreach(println)
+
+  }
+
+}
